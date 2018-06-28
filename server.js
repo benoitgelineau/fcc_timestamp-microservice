bec5bd2b07519ec3fc7cc7c3adc9da53c9f91cdf
Benoît Gelineau <39692884+benoitgelineau@users.noreply.github.com>
2018-06-28 11:45:32 +0200

    Make API work

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
@@ -10,23 +7,28 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// API endpoint
+app.get('/api/timestamp/:date_string?', (req, res, next) => {
+    req.date_string = /\D/.test(req.params.date_string) ? req.params.date_string : parseInt(req.params.date_string);
+    req.date = req.date_string ? new Date(req.date_string) : new Date();
+    req.valid = req.date.getTime() ? true : false;
+    next();
+  }, (req, res) => {
+    if (!req.valid) {
+      res.json({ error: "Invalid Date" })
+    } else {
+      res.json({ unix: /\D/.test(req.date_string) ? req.date.getTime() : req.date_string, utc: req.date.toUTCString() })
+    }
+  }
+);
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
+// listen for requests
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
